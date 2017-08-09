41f56f454bbd5acbc782ea9974dbbe3207f3fb2a
RettBehrens <everettbehrens@gmail.com>
2017-08-09 16:23:04 -0600

    changed how the yelp link is displayed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,3 @@
 $(document).ready(function() {
-	$('#businesses').on('click', '.add-business', function(event) {
-	//var id= $(this).parents('.album').data('album-id');
-	//$('#songModal').data('album-id', id);
-	console.log($('#addBusinessModal'));
-	$('#addBusinessModal').modal();
-	});
+	
 });
-
-
-
-
-
-
-
-// $('#searchForm').on('submit', function(event) {
-	// 	event.preventDefault();
-	// 	$.ajax({
-	// 		method: 'GET',
-	// 		url: '/searchResults',
-	// 		data: {
-	// 			businessName: $('#businessName').val(),
-	// 			businessCity: $('#businessCity').val()
-	// 		},
-	// 		success: function(results){
-	// 			console.log(results);
-	// 		}
-	// 	});
-	// });
