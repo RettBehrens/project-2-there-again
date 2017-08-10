332dd6045971f36c6584d0fba9ab13791e80f3b7
RettBehrens <everettbehrens@gmail.com>
2017-08-10 07:57:19 -0600

    ditto for NOT There Again button click

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,8 @@ function handleError(json) {
 
 $(document).ready(function() {
 	console.log('this is app.js');
+	
+	//"There Again" button
 	$('.there-again').on('click', function() {
 		var businessName = $(this).closest('.business').find('.business-name').html();
 		var businessImageURL = $(this).closest('.business').find('img').attr('src');
@@ -31,8 +33,31 @@ $(document).ready(function() {
 			success: handleSuccess,
 			error: handleError
 		});
+	});
 
+	//"NOT There Again" button
+	$('.not-there-again').on('click', function() {
+		var businessName = $(this).closest('.business').find('.business-name').html();
+		var businessImageURL = $(this).closest('.business').find('img').attr('src');
+		var businessYelpURL = $(this).closest('.business').find('.business-yelp-link a').attr('href');
+		var businessAddress = $(this).closest('.business').find('.business-address').html();
+		var thereAgain = false;
+		
+		var businessToSave = {
+			businessName: businessName,
+			businessImageURL: businessImageURL,
+			businessYelpURL: businessYelpURL,
+			businessAddress: [businessAddress],
+			thereAgain: thereAgain
+		};
+		console.log(businessToSave);
 
-		//6.AJAX POST this data
+		$.ajax({
+			method: 'post',
+			url: '/profile',
+			data: businessToSave,
+			success: handleSuccess,
+			error: handleError
+		});
 	});
 });
